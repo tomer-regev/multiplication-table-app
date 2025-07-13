b7dedb02c83d0e641ed59ba5f6df67d6cc88eef7
tomer.regev@zoominfo.com <tomer.regev@zoominfo.com>
2025-07-13 14:38:09 +0300

    fix fb

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,5 +1,5 @@
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDH9pSv-1acC0eIZgUDTUYZn7S0JgiKkwA",
   authDomain: "multiplication-table-app-5fa66.firebaseapp.com",
   projectId: "multiplication-table-app-5fa66",
